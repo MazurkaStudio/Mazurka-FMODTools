75677ca3d33f689c73f4372d0675990130fc3de6
Millers Lilian <mrmillerslilian@outlook.fr>
2022-11-01 00:31:39 +0100

    error

--- Runtime/FMODGizmos.cs
+++ Runtime/FMODGizmos.cs
@@ -1,6 +1,7 @@
 using FMOD.Studio;
 using FMODUnity;
 using MazurkaGameKit.FMODTools;
+using Newtonsoft.Json.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,15 +12,13 @@ namespace MazurkaGameKit.FMODTools
     {
         public static void VisuzalizeSound3DAttributs(EventReference eventRef, Transform soundSource)
         {
-            if (FMODHelper.GetEventDescription(eventRef, out EventDescription eventDescription))
+            if (FMODHelper.GetEventDescription(eventRef).getMinMaxDistance(out float min, out float max) == FMOD.RESULT.OK)
             {
-                if (eventDescription.getMinMaxDistance(out float min, out float max) == FMOD.RESULT.OK)
-                {
-                    Gizmos.color = Color.blue;
-                    Gizmos.DrawWireSphere(soundSource.position, min);
-                    Gizmos.DrawWireSphere(soundSource.position, max);
-                }
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(soundSource.position, min);
+                Gizmos.DrawWireSphere(soundSource.position, max);
             }
+
         }
     }
 }
