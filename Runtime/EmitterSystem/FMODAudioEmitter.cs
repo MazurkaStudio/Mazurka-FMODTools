c3c076026030ca20c38e5620c219ffcecffe8128
Millers Lilian <mrmillerslilian@outlook.fr>
2022-11-02 19:37:18 +0100

    Fix emitter canEmit

--- Runtime/EmitterSystem/FMODAudioEmitter.cs
+++ Runtime/EmitterSystem/FMODAudioEmitter.cs
@@ -50,6 +50,8 @@ public abstract class FMODAudioEmitter : MonoBehaviour, IFMODAudioEmitter
                 {
                     StopEmitter();
                 }
+
+                canEmitSound = value;
             }
         }
     }
